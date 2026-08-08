b29f8bdec7c262b4b77d724aed26830298dd4639
shota19900803-stack <shota.19900803@gmail.com>
2026-08-08 21:46:40 +0900

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -572,8 +572,8 @@ const product = products.find(
       await supabase
         .from("products")
         .update({
-         stock_quantity:
-  product.stock_quantity - quantity,
+  stock_quantity:
+  product.stock_quantity - saleForm.quantity,
         })
         .eq("id", product.id);
     }
