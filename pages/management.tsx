e799558dfb4de7c7d790c9d2f9485518c34e5c64
shota19900803-stack <shota.19900803@gmail.com>
2026-08-25 08:08:16 +0900

    Prevent Supabase management page from server build evaluation

--- pages/management.tsx
+++ pages/management.tsx
@@ -1,4 +1,9 @@
-import Management from "../components/Management";
+import dynamic from "next/dynamic";
+
+const Management = dynamic(() => import("../components/Management"), {
+  ssr: false,
+  loading: () => <main style={{ padding: 40, fontFamily: "system-ui" }}>経営システムを読み込んでいます…</main>,
+});
 
 export default function ManagementPage() {
   return <Management />;
